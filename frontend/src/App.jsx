c839c4e891e2bbd955dc5c245b3da118cfdac224
luka gulverdashvili <lukagulverdashvili49@gmail.com>
2024-08-10 00:11:22 +0400

    Added BG and change tasks

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import AppRoutes from './Routes/AppRoutes'; // Renamed to 'AppRoutes'
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className="relative">
+        <h1 className="absolute">Task Manager</h1>
+      </div>
+      <AppRoutes />
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/Routes/AppRoutes.jsx
@@ -0,0 +1,69 @@
+import React, { useState } from 'react';
+import { Route, Routes, useNavigate } from 'react-router-dom';
+import Login from '../components/Login';
+import Signout from '../components/Signout';
+import AddTask from '../components/AddTask';
+import TaskList from '../components/TaskList';
+
+function AppRoutes() {
+  const [token, setToken] = useState('');
+  const navigate = useNavigate();
+
+  const handleAddTaskClick = () => {
+    navigate('/addtask');
+  };
+
+  return (
+    <div>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            token ? (
+              <div>
+                <button className="button" onClick={handleAddTaskClick}>Add Task</button>
+                <TaskList token={token} />
+                <Signout token={token} setToken={setToken} />
+              </div>
+            ) : (
+              <Login setToken={setToken} />
+            )
+          }
+        />
+
+        <Route
+          path="/signout"
+          element={<Signout token={token} setToken={setToken} />}
+        />
+
+        <Route
+          path="/addtask"
+          element={
+            token ? (
+              <AddTask token={token} />
+            ) : (
+              <Login setToken={setToken} />
+            )
+          }
+        />
+
+        <Route
+          path="/profile"
+          element={
+            token ? (
+              <div>
+                <button className="button" onClick={handleAddTaskClick}>Add Task</button>
+                <TaskList token={token} />
+                <Signout token={token} setToken={setToken} />
+              </div>
+            ) : (
+              <Login setToken={setToken} />
+            )
+          }
+        />
+      </Routes>
+    </div>
+  );
+}
+
+export default AppRoutes; // Renamed export
